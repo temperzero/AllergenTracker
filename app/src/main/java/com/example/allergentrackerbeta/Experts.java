b3f0f3fd1c00241ca010bfb7174c799bfe3b3f43
temperzero <51377166+temperzero@users.noreply.github.com>
2022-09-11 14:36:31 +0300

    deleted prefrences and unused layouts. fixed meuhedet

--- app/src/main/java/com/example/allergentrackerbeta/Experts.java
+++ app/src/main/java/com/example/allergentrackerbeta/Experts.java
@@ -68,13 +68,13 @@ public class Experts extends AppCompatActivity {
             }
         });
 
-        meuhedet = findViewById(R.id.yahel);
+        meuhedet = findViewById(R.id.meuhedet);
         meuhedet.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                Intent openlinkmeuhedet = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.meuhedet.co.il/search?mod=115&rn=18716%22%7D"));
+                Intent openlinkmeuhedet = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.meuhedet.co.il/%D7%96%D7%9B%D7%95%D7%99%D7%95%D7%AA-%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D-%D7%95%D7%91%D7%99%D7%98%D7%95%D7%97%D7%99%D7%9D/%D7%96%D7%9B%D7%95%D7%99%D7%95%D7%AA-%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D-%D7%95%D7%91%D7%99%D7%98%D7%95%D7%97%D7%99%D7%9D/%D7%98%D7%99%D7%A4%D7%95%D7%9C%D7%99%D7%9D/%D7%91%D7%99%D7%A7%D7%95%D7%A8-%D7%90%D7%A6%D7%9C-%D7%A8%D7%95%D7%A4%D7%90-%D7%9E%D7%95%D7%9E%D7%97%D7%94-%D7%9C%D7%90%D7%9C%D7%A8%D7%92%D7%99%D7%94-%D7%95%D7%90%D7%99%D7%9E%D7%95%D7%A0%D7%95%D7%9C%D7%95%D7%92%D7%99%D7%94/?q=%D7%90%D7%9C%D7%A8%D7%92%D7%99%D7%94"));
                 startActivity(openlinkmeuhedet);
             }
         });
